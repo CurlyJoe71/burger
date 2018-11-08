1dcb242dbae2387075cd84c9dae30ea5f62d5c02
Curly Joe <curlyjoe71@gmail.com>
2018-11-07 18:12:41 -0700

    Updated mysql

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Jmg070181mu$ic",
-    database: "burgers_db"
-});
+
+
+if (process.env.JAWSB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    const connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Jmg070181mu$ic",
+        database: "burgers_db"
+    });
+};
 
 connection.connect(function (err) {
     if (err) {
